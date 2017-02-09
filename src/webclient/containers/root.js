88bcb930c3645404d3239940cf76be64c7cc5e16
David <davesharff@gmail.com>
2017-02-09 02:18:39 -0600

    Create live-props container and silly breakout of live prop component*
    * I did this to have a working example of "dumb" components vs. containers (top-level views connected to the store)

--- src/webclient/containers/root.js
+++ src/webclient/containers/root.js
@@ -1,69 +1,15 @@
 'use strict';
 const React = require('react');
-const {connect} = require('react-redux');
-const autobind = require('autobind-decorator');
-const socket = require('../socket');
-const {ADD_PROP} = require('../../shared/action-types');
-const {createRandomId} = require('../../shared/utils');
+const LiveProps = require('./live-props');
 
-const {PropTypes} = React;
-@connect((state) => ({
-  liveProps: state.liveProps || []
-}))
-@autobind
 class Root extends React.Component {
-  static propTypes = {
-    liveProps: PropTypes.arrayOf(PropTypes.object)
-  }
-
-  state = {
-    newPropDescription: null
-  }
-
-  handleNewPropInput(e) {
-    this.setState({
-      newPropDescription: e.target.value
-    });
-  }
-
-  handleSubmitNewProp(e) {
-    e.preventDefault();
-    if (this.state.newPropDescription) {
-      socket.sendAction({
-        type: ADD_PROP,
-        payload: {
-          id: createRandomId(),
-          description: this.state.newPropDescription
-        }
-      });
-      this.setState({
-        newPropDescription: null
-      });
-    }
-  }
-
   render() {
     return (
       <div style={rootContainerStyle}>
-        <div>
+        <div style={titleStyle}>
           Welcome to the Gonfalon Sports Book!
         </div>
-        <div>
-          <div>
-            <input
-              type="text"
-              value={this.state.newPropDescription}
-              onChange={this.handleNewPropInput}
-            />
-            <button onClick={this.handleSubmitNewProp}>Add Prop</button>
-          </div>
-          <div>Current Props</div>
-          {
-            this.props.liveProps.map(({id, description}) =>
-              <div key={id}>{description}</div>
-            )
-          }
-        </div>
+        <LiveProps />
       </div>
     );
   }
@@ -76,4 +22,9 @@ const rootContainerStyle = {
   flexDirection: 'column',
   fontFamily: 'Helvetica Neue,Helvetica,Arial,sans-serif',
   fontWeight: '300'
+};
+
+// TODO: move common shared styles into styles.js
+const titleStyle = {
+  padding: '10px'
 };
