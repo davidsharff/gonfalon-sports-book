6239e18d882079925967dd0cd99a14dd539f9b0c
David <davesharff@gmail.com>
2017-02-09 01:24:48 -0600

    Remove redux devtools until it's worth the time sink to configure

--- src/webclient/index.js
+++ src/webclient/index.js
@@ -10,15 +10,11 @@ const {Provider} = require('react-redux');
 const store = require('./store');
 const Root = require('./containers/Root');
 const socket = require('./socket');
-// TODO: toggle require when we support prod vs dev mode.
-const DevTools = require('./components/devtools/devtools');
 
 socket.onMessage(({data: action}) => store.dispatch(JSON.parse(action)));
 
 ReactDOM.render((
   <Provider store={store}>
-    <Root>
-      {DevTools ? <DevTools /> : null}
-     </Root>
+    <Root />
   </Provider>
 ), document.getElementById('app'));
